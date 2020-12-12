c5a64fe0bd290c1e44a23c95dc72f30d7d1e488a
Vadim Karelov <vadim.karelov.02@mail.ru>
2020-12-12 12:25:57 +0500

    Добавьте файлы проекта.

---
+++ LoboratoryWorkRecursion/LoboratoryWorkRecursion.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+double Koren(double a, int n)
+{
+    if (n == 0)
+        return 0.5 * (1 + a);
+    double x = Koren(a, n - 1);
+    return 0.5 * (x + a / x);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n, eps;
+    do
+    {
+        cout << "Введите целое число >= 0" << endl;
+        cin >> n;
+        cout << "Введите точность - колво раз работы рекурсивной функции" << endl;
+        cin >> eps;
+    } while (n < 0 || eps <= 0);
+    cout << "Корень из " << n << " = " << Koren(n, eps);
+}
